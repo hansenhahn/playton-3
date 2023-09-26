b74ed7605cfa34b759b9e87ca2dac1e425b4c398
DiegoHH <hansen.hahn@gmail.com>
2023-09-26 15:23:13 -0300

    Imagens e textos traduzidos.

--- Programas/unpack_images_en.bat
+++ Programas/unpack_images_en.bat
@@ -1,9 +1,9 @@
 @echo Monkeys Traduções
 
 @echo "Calling tl_img.py to unpack background"
-rem pypy tl_img.py -m e0 -s "../ROM Original/PLAYTON3/data/lt3" -d "../Imagens Originais/bg"
+pypy tl_img.py -m e0 -s "../ROM Original/PLAYTON3/data/lt3" -s1 "../Arquivos Originais/uk/bg" -d "../Imagens Originais/uk/bg"
 
 REM echo "Calling tl_img.py to unpack animation"
-pypy tl_img.py -m e1 -s "../ROM Original/PLAYTON3/data/lt3" -d "../Imagens Originais/ani"
+rem pypy tl_img.py -m e1 -s "../ROM Original/PLAYTON3/data/lt3" -d "../Imagens Originais/ani"
 
 pause
--- ROM Original/unpack_rom.bat
+++ ROM Original/unpack_rom.bat
@@ -1,6 +1,6 @@
 @echo off
 
-md PLAYTON3
-cd PLAYTON3
-..\ndstool -x "..\5295_-_Professor_Layton_and_the_Lost_Future_EUR_NDS.nds" -9 arm9.bin -7 arm7.bin -y9 y9.bin -y7 y7.bin -d data -y overlay -t banner.bin -h header.bin
+md PLAYTON3_IT
+cd PLAYTON3_IT
+..\ndstool -x "..\5284 - Professor Layton e il Futuro Perduto, Il (Italy) [b].nds" -9 arm9.bin -7 arm7.bin -y9 y9.bin -y7 y7.bin -d data -y overlay -t banner.bin -h header.bin
 
